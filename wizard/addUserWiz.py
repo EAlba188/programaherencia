b74a388176460b3b7fdbacec8168d649f62dbb5d
EAlba188 <edenalba188@gmail.com>
2021-04-28 12:35:20 +0200

    4.0

--- wizard/addUserWiz.py
+++ wizard/addUserWiz.py
@@ -6,8 +6,21 @@ from odoo import models, fields, api
 class addUserWiz(models.TransientModel):
     _name = "wizard.cambiousuario"
 
-    theOne = fields.Many2one("res.users", string="Elegido")
+    theOne = fields.Many2one("res.users", string="Comercial")
+
+    theClient = fields.Many2one("res.partner", string="Cliente")
 
     def setChoosenOnes(self):
         for x in self.env['sale.order'].browse(self.env.context.get('active_ids')):
             x.user_id = self.theOne
+            x.partner_id = self.theClient
+
+class tipoInicial(models.Model):
+    _name = "tipo.inicial"
+
+    name = fields.Char(string="Nombre")
+
+class heredaTickets(models.Model):
+    _inherit = "helpdesk.ticket"
+
+    tipo_inicial = fields.Many2one("tipo.inicial", string="Tipo Inicial")
